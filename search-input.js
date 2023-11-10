4a3d33abfac8cd5bf2cfc2ba041a1477f3628a89
connor <caschm@gmail.com>
2023-11-10 13:21:52 -0500

    Add initial javascript

--- search-input.js
+++ search-input.js
@@ -1,4 +1,4 @@
-const input = document.querySelector("#fruit");
+const input = document.querySelector("#search-input");
 const suggestions = document.querySelector(".suggestions ul");
 
 const fruit = [
@@ -83,25 +83,107 @@ const fruit = [
     "Yuzu",
 ];
 
-function search(str) {
-    let results = [];
-
-    // TODO
-
-    return results;
-}
+input.addEventListener("keyup", searchHandler);
+suggestions.addEventListener("click", useSuggestion);
 
 function searchHandler(e) {
-    // TODO
+    // When text is entered in the search input...
+
+    // Store the text into a variable
+    const textInput = e.target;
+    let searchText = textInput.value.trim();
+    let fruitResults = [];
+    console.log(searchText);
+
+    // clear suggestions after each update:
+    clearSuggestions();
+
+    // If input is at least 2 chars create a filtered list of fruit
+    if (searchText.length > 1) {
+        fruitResults = search(searchText);
+        // console.log(fruitResults);
+    }
+
+    // If filterFruit has items, show suggestions
+    if (fruitResults) {
+        showSuggestions(fruitResults, searchText);
+    }
+}
+
+function search(searchText) {
+    const searchTextLower = searchText.toLowerCase();
+    return fruit.filter((value) => value.toLowerCase().includes(searchTextLower));
 }
 
 function showSuggestions(results, inputVal) {
-    // TODO
+    // create an li for each of the results
+    console.log(`Results sent to suggestion LIs: ${results}`);
+    for (let result of results) {
+        const resultItem = document.createElement("li");
+        resultItem.classList.add("result-item");
+
+        // add hover event listener to addd style of suggestion on hover
+        resultItem.addEventListener("mouseover", (e) =>
+            resultItem.classList.toggle("result-hovered")
+        );
+        // add hover event listener to remove style of suggestion when mouse leaves
+        resultItem.addEventListener("mouseout", (e) =>
+            resultItem.classList.toggle("result-hovered")
+        );
+
+        const resultItemContent = generateResultItemContent(result, inputVal);
+        resultItem.innerHTML = resultItemContent;
+
+        suggestions.append(resultItem);
+    }
+}
+
+function clearSuggestions() {
+    // Q: Would a better alternative be to update the suggestion list to remove items that no longer match?
+    // Q: How to make this function return something instead of sideeffects on global var?
+    while (suggestions.firstChild) {
+        suggestions.removeChild(suggestions.lastChild);
+    }
+}
+
+function generateResultItemContent(result, inputVal) {
+    // generates bolded text for the inputVal on the result
+    const resultLowered = result.toLowerCase();
+    const inputLowered = inputVal.toLowerCase();
+    const inputIndex = resultLowered.indexOf(inputLowered);
+    let resultHTML = "";
+
+    if (inputIndex >= 0) {
+        resultHTML =
+            result.slice(0, inputIndex) +
+            "<b>" +
+            result.slice(inputIndex, inputIndex + inputVal.length) +
+            "</b>" +
+            result.slice(inputIndex + inputVal.length);
+    } else {
+        throw new Error(
+            "Couldn't generate suggestion text. Input value not found in result string."
+        );
+    }
+
+    return resultHTML;
 }
 
 function useSuggestion(e) {
-    // TODO
-}
+    // when a suggestion is clicked...
 
-input.addEventListener("keyup", searchHandler);
-suggestions.addEventListener("click", useSuggestion);
+    let suggestion = e.target;
+
+    // if bold area clicked, update the target to be the parent li
+    if (suggestion.nodeName !== "LI") {
+        suggestion = suggestion.parentElement;
+    }
+
+    // update the value of the input to be the value of the clicked suggestion
+    const suggestionText = suggestion.innerText;
+    console.log(`Suggestion clicked: ${suggestionText}`);
+    input.value = suggestionText;
+
+    // hide the suggestions ul
+    clearSuggestions();
+}
